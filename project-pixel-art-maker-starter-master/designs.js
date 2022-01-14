31f5fd34e059cddc9fec932092897fa29f24dfee
Rume Gbudje <94413857+Rumekhan@users.noreply.github.com>
2022-01-13 14:18:32 -1200

    Add files via upload

---
+++ project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,10 @@
+// Select color input
+// Select size input
+var width, height, color;
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
